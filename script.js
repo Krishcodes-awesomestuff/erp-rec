50545a1cfa202eddb78be23c3907b88b6a4b5516
sivaplaysmC <sivaram.asdf@gmail.com>
2025-04-02 21:15:24 +0530

    form - save form responses to DB

--- script.js
+++
@@ -1,8 +0,0 @@
-function generateCaptcha() {
-    const digits = '0123456789';
-    let captcha = '';
-    for(let i = 0; i < 4; i++) {  // Changed to exactly 4 digits
-        captcha += digits[Math.floor(Math.random() * 10)] + ' ';
-    }
-    return captcha.trim();
-}
